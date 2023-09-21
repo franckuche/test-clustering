2b3ac4f21bfe0fb75e0ddba8474ab790e0e991c7
Franck Mairot <franck.mairot@gmail.com>
2023-09-21 15:44:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,18 +8,20 @@ st.set_page_config(page_title="Extracteur de Domaines", layout="wide")
 st.title("Extracteur de noms de domaine à partir des résultats de recherche")
 st.write("Cette application permet d'extraire les noms de domaine des deux premiers résultats de recherche de Google pour chaque mot-clé fourni.")
 
-def fetch_domains(keyword):
+def fetch_links(keyword):
     url = f"https://api.spaceserp.com/google/search?apiKey=8e87e954-6b75-4888-bd6c-86868540beeb&q={keyword}&domain=google.fr&gl=cn&hl=nl&device=mobile"
     
     response = requests.get(url)
     data = response.json()
     
     if 'organic_results' in data:
-        urls = [entry.get('link', '') for entry in data['organic_results'] if 'link' in entry][:2]
-        domains = [urlparse(url).netloc for url in urls]
-        return ", ".join(domains)
+        urls = [entry.get('link', '') for entry in data['organic_results'] if 'link' in entry][:20]
+        return ", ".join(urls)
     else:
         return "N/A"
+
+st.title("Extracteur de liens à partir des résultats de recherche")
+st.write("Cette application permet d'extraire les liens des 20 premiers résultats de recherche de Google pour chaque mot-clé fourni.")
 
 uploaded_file = st.file_uploader("📤 Choisissez un fichier CSV contenant vos mots-clés", type="csv")
 
@@ -30,13 +32,13 @@ if uploaded_file is not None:
 
     if 'keyword' in df.columns:
         st.subheader("Extraction en cours...")
-        df['top_2_domains'] = df['keyword'].apply(fetch_domains)
+        df['cluster'] = df['keyword'].apply(fetch_links)
 
-        st.subheader("Résultats avec les noms de domaine extraits")
+        st.subheader("Résultats avec les liens extraits")
         st.write(df)
         
         # Option pour télécharger le CSV mis à jour
         csv_download = df.to_csv(index=False).encode()
-        st.download_button("Télécharger le CSV avec noms de domaine", csv_download, "updated_keywords.csv")
+        st.download_button("Télécharger le CSV avec les liens", csv_download, "updated_keywords.csv")
     else:
         st.error("❌ Le fichier CSV doit avoir une colonne 'keyword'")
